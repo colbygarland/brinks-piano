72dee58ec011e3fd87747a54900f4c237e091b66
Colby Garland <colbygarland@gmail.com>
2020-11-30 21:37:02 -0700

    Added covid banner message

--- components/CovidBanner.vue
+++ components/CovidBanner.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="bg-primary px-2 py-2">
     <p class="text-white text-center">
-      <small>Due to COVID-19 information goes here</small>
+      <small
+        >We are scheduling appointments! Extra health and safety precautions
+        will be taken such as masks and gloves where needed/requested,
+        sanitizing keyboards upon completion of work, and maintaining
+        appropriate social distance protocols. We look forward to serving your
+        piano needs.</small
+      >
     </p>
   </div>
 </template>

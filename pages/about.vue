26c4b50252a564523053458243a433cde23fd9ab
Colby Garland <colbygarland@gmail.com>
2020-11-30 19:03:34 -0700

    Updated content

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,51 @@
 <template>
-    <div>
-        <Content>
-            <Container>
-                <H1>About Brink's Piano Works</H1>
-                <div class="prose">
-                    <p>Steve Vanden Brinks founded Brink's Piano Works in 2019, after realizing a need in the Peace region for quality instrument repair and restoration.</p>
-                </div>
-            </Container>
-        </Content>
-    </div>
+  <div>
+    <Content>
+      <Container>
+        <H1>About Brink's Piano Works</H1>
+        <div class="prose">
+          <div class="lg:flex">
+            <div>
+              <img :src="Steve" alt="Steve Vanden Brink" class="pr-6" />
+            </div>
+            <div class="lg:mt-10 xl:mt-20">
+              <p>
+                At Brink’s Piano works, I have a vision for instruments to work
+                and sound their very best, and as such I have surrounded myself
+                with two experienced Technicians.
+              </p>
+              <p>
+                I have joined international forums, am taking two courses, and
+                am apprenticing under Grande Prairie’s best guitar tech.
+              </p>
+              <p>
+                One can never stop learning, and so this is my goal, to strive
+                to excellence in all things relating to instruments, whether it
+                is tuning a piano, or fixing a ukulele.
+              </p>
+              <p>
+                I am a father, and husband, and I believe in passing on beauty,
+                joy, and the sheer delight of music in all forms to the
+                generations after my own. Investing in music is, to me, one of
+                the most worthwhile things we can do. Because music is a
+                language that all people speak, and understand. We make music,
+                better.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </Content>
+  </div>
 </template>
+
+<script>
+import Steve from "~/assets/images/Steve.jpg";
+export default {
+  data() {
+    return {
+      Steve,
+    };
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,119 @@
 <template>
-    <div>
-        <div class="pt-16 lg:p-0 lg:flex lg:justify-between">
-            <div class="text-center lg:text-left lg:w-1/2 lg:py-32 xl:py-64 lg:px-20">
-                <div class="prose px-6 lg:p-0">
-                    <div class="max-w-xl">
-                        <h1 class="font-serif">Brink's Piano Works</h1>
-                        <p>From piano tunings, to guitar set-ups, Brink's Piano Works is here to help you keep your instrument sounding as great as you play it.</p>
-                    </div>
-                </div>
-                <div class="flex justify-center lg:justify-start mt-5 mb-5">
-                    <nuxt-link to="/services" class="mr-5 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded font-serif">Our Services</nuxt-link>
-                    <nuxt-link to="/about" class="bg-transparent hover:bg-black hover:text-white text-black font-bold py-2 px-4 rounded font-serif">About Us</nuxt-link>
-                </div>
-                <img class="mt-10 lg:hidden" :src="piano" alt="Piano" />
-            </div>
-            <div class="hidden lg:w-1/2 lg:block lg:bg-cover lg:bg-no-repeat lg:bg-center lg:min-h-full" :style="{ backgroundImage: `url(${piano})` }"></div>
+  <div>
+    <div class="pt-16 lg:p-0 lg:flex lg:justify-between">
+      <div class="text-center lg:text-left lg:w-1/2 lg:py-32 xl:py-64 lg:px-20">
+        <div class="prose px-6 lg:p-0">
+          <div class="max-w-xl">
+            <h1 class="font-serif">Brink's Piano Works</h1>
+            <p>
+              From piano tunings, to guitar set-ups, Brink's Piano Works is here
+              to help you keep your instrument sounding as great as you play it.
+            </p>
+          </div>
         </div>
-
-        <ContentBackground>
-            <Container>
-                <H2 class="text-center">Our Services</H2>
-            </Container>
-        </ContentBackground>
-
-        <div class="px-6 py-10 lg:py-16">
-            <div class="container mx-auto">
-                <div class="lg:flex">
-                    <div class="mb-12 lg:mb-0 lg:px-4">
-                        <h3 class="font-serif text-xl font-bold mb-4">Pianos</h3>
-                        <p>We offer a wide variety of piano services, from tuning, to string replacements, to Grand Piano Regulation.</p>
-                        <nuxt-link to="/services#piano" class="inline-block mt-3 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded font-serif">Learn more</nuxt-link>
-                    </div>
-                    <div class="mb-12 lg:mb-0 lg:px-4">
-                        <h3 class="font-serif text-xl font-bold mb-4">Instrument Set Ups</h3>
-                        <p>From violins to guitars, and all in between, we pride ourselves in our ability to set up your instruments.</p>
-                        <nuxt-link to="/services#other" class="inline-block mt-3 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded font-serif">Learn more</nuxt-link>
-                    </div>
-                    <div class="mb-12 lg:mb-0 lg:px-4">
-                        <h3 class="font-serif text-xl font-bold mb-4">Instrument Repairs</h3>
-                        <p>Pianos, guitars, you name it! Brinks Piano Works can repair your instrument.</p>
-                        <nuxt-link to="/services#other" class="inline-block mt-3 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded font-serif">Learn more</nuxt-link>
-                    </div>
-                </div>
-            </div>
+        <div class="flex justify-center lg:justify-start mt-5 mb-5">
+          <nuxt-link
+            to="/services"
+            class="mr-5 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded font-serif"
+            >Our Services</nuxt-link
+          >
+          <nuxt-link
+            to="/about"
+            class="bg-transparent hover:bg-black hover:text-white text-black font-bold py-2 px-4 rounded font-serif"
+            >About Us</nuxt-link
+          >
         </div>
-
-        <div class="lg:flex">
-            <div class="lg:w-1/3 h-50 bg-center bg-cover bg-no-repeat" :style="{ backgroundImage: `url(${piano2})` }"></div>
-            <div class="lg:w-1/3 h-50 bg-center bg-cover bg-no-repeat" :style="{ backgroundImage: `url(${piano3})` }"></div>
-            <div class="lg:w-1/3 h-50 bg-center bg-cover bg-no-repeat" :style="{ backgroundImage: `url(${piano4})` }"></div>
-        </div>
+        <img class="mt-10 lg:hidden" :src="piano" alt="Piano" />
+      </div>
+      <div
+        class="hidden lg:w-1/2 lg:block lg:bg-cover lg:bg-no-repeat lg:bg-center lg:min-h-full"
+        :style="{ backgroundImage: `url(${piano})` }"
+      ></div>
     </div>
+
+    <ContentBackground>
+      <Container>
+        <H2 class="text-center">Our Services</H2>
+      </Container>
+    </ContentBackground>
+
+    <div class="px-6 py-10 lg:py-16">
+      <div class="container mx-auto">
+        <div class="lg:flex">
+          <div class="mb-12 lg:mb-0 lg:px-4">
+            <h3 class="font-serif text-xl font-bold mb-4">Pianos</h3>
+            <p>
+              We offer a wide variety of piano services, from tuning, to string
+              replacements, to Grand Piano Regulation.
+            </p>
+            <nuxt-link
+              to="/services#piano"
+              class="inline-block mt-3 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded font-serif"
+              >Learn more</nuxt-link
+            >
+          </div>
+          <div class="mb-12 lg:mb-0 lg:px-4">
+            <h3 class="font-serif text-xl font-bold mb-4">
+              Instrument Set Ups
+            </h3>
+            <p>
+              From violins to guitars, and all in between, we pride ourselves in
+              our ability to set up your instruments.
+            </p>
+            <nuxt-link
+              to="/services#other"
+              class="inline-block mt-3 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded font-serif"
+              >Learn more</nuxt-link
+            >
+          </div>
+          <div class="mb-12 lg:mb-0 lg:px-4">
+            <h3 class="font-serif text-xl font-bold mb-4">
+              Instrument Repairs
+            </h3>
+            <p>
+              Pianos, guitars, you name it! Brinks Piano Works can repair your
+              instrument.
+            </p>
+            <nuxt-link
+              to="/services#other"
+              class="inline-block mt-3 bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded font-serif"
+              >Learn more</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="lg:flex">
+      <div
+        class="lg:w-1/3 h-50 bg-center bg-cover bg-no-repeat"
+        :style="{ backgroundImage: `url(${piano2})` }"
+      ></div>
+      <div
+        class="lg:w-1/3 h-50 bg-center bg-cover bg-no-repeat"
+        :style="{ backgroundImage: `url(${piano3})` }"
+      ></div>
+      <div
+        class="lg:w-1/3 h-50 bg-center bg-cover bg-no-repeat"
+        :style="{ backgroundImage: `url(${piano4})` }"
+      ></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import piano from '~/assets/images/piano.jpg';
-import piano2 from '~/assets/images/piano3.jpg';
-import piano3 from '~/assets/images/piano-with-antlers.jpg';
-import piano4 from '~/assets/images/keys.jpg';
+import piano from "~/assets/images/grand-piano.jpg";
+import piano2 from "~/assets/images/piano3.jpg";
+import piano3 from "~/assets/images/piano-with-antlers.jpg";
+import piano4 from "~/assets/images/keys.jpg";
 export default {
-    data() {
-        return {
-            piano,
-            piano2,
-            piano3,
-            piano4,
-        }
-    }
-}
+  data() {
+    return {
+      piano,
+      piano2,
+      piano3,
+      piano4,
+    };
+  },
+};
 </script>

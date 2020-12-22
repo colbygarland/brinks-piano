3b1e7b47021e33ab3d69661dd22b759213f918cb
Colby Garland <colbygarland@gmail.com>
2020-12-22 11:07:55 -0700

    Remove needed from covid banner. Closes #13

--- components/CovidBanner.vue
+++ components/CovidBanner.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="bg-primary px-2 py-2">
-    <p class="text-white text-center">
-      <small
-        >We are scheduling appointments! Extra health and safety precautions
-        will be taken such as masks and gloves where needed/requested,
-        sanitizing keyboards upon completion of work, and maintaining
-        appropriate social distance protocols. We look forward to serving your
-        piano needs.</small
-      >
-    </p>
-  </div>
+    <div class="bg-primary px-2 py-2">
+        <p class="text-white text-center">
+            <small
+                >We are scheduling appointments! Extra health and safety
+                precautions will be taken such as masks and gloves where
+                requested, sanitizing keyboards upon completion of work, and
+                maintaining appropriate social distance protocols. We look
+                forward to serving your piano needs.</small
+            >
+        </p>
+    </div>
 </template>
